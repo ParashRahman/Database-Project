5e38c9946b93f1ea3ef3a5eb40e4ec773f56f93e
ParashRahman <parash@ualberta.ca>
2015-03-21 13:35:50 -0600

    Error checker is not tested but you can get the format from it. Testing now.

--- Applications/error_checker.py
+++ Applications/error_checker.py
@@ -1,13 +1,80 @@
 import cx_Oracle
+import decimal
+import datetime
 
 class ErrorChecker:
+    # to call this function import ErrorChecker and call error_checker.ErrorChecker.check_error( respective parameters )
+    # returns True if data is valid
+    # returns False otherwise
+
+    """
+
+    To check if DATETIME/DATE objects are correct: 
+    meta_data_fragment: same as usual
+    user_input: tuple ( DD, MM, YYYY ) where DD, MM, and YYYY are integers
+    If you want to check if DD, MM, or YYYY are integers use the check_if_string_is_integer() function
+    
+    """
     def check_error( meta_data_fragment, user_input ):
-        # First check if the types are valid
+        # Check if empty string is ok
+        null_ok = meta_data_fragment[6]
+        if ( null_ok and len(user_input) == 0 ):
+            return True
+
+        # Get the data_type to do the proper check
         data_type = meta_data_fragment[1]
+
+        # Check the integer type
         if type(data_type) == cx_Oracle.NUMBER:
             try:
                 int( user_input )
             except (ValueError): 
                 return False
+            
+            precision = meta_data_fragment[4]
+            
+            # integer types should not have any precision
+            if ( precision == 0 ):
+                float_user_input = float( user_input )
+                int_user_input = int( user_input )
+                if ( float_user_input > int_user_input ):
+                    return False
+            # check if float type has proper precision
+            else:
+                decimal_user_input = decimal.Decimal( user_input )
+                uinput_precision = decimal_user_input.as_tuple().exponent
+                if ( abs ( uinput_precision ) >= precision ):
+                    return False
 
+            # integer types should correspond to the internal size
+            internal_size = meta_data_fragment[3]
+            if ( 10**internal_size < user_input ):
+                return False
+
+        # Check string objects
+        elif type(data_type) == cx_Oracle.STRING:
+            desired_length = metad_data_fragment[3]
+            if ( len( user_input ) > desired_length ):
+                return False
+            
+        # Check datetime objects
+        elif type(data_type) == cx_Oracle.DATETIME:
+            try:
+                dd = user_input[0]
+                mm = user_input[1]
+                yyyy = user_input[2]
+                date = datetime.datetime( yyyy, mm, dd )
+            except( ValueError ):
+                return False
+
+
+        return True
+
+
+    def check_if_string_is_integer( string ):
+        try:
+            int( string )
+        except( ValueError ): 
+            return False
         
+        return True
--- Applications/record_violation.py
+++ Applications/record_violation.py
@@ -4,71 +4,75 @@ class RecordViolation(Application):
     def start_application(self, c):
         self.cursor = c
         
-        fields = [ "Ticket no.",
-                   "Violator no.",
-                   "Vehicle id",
-                   "Office no.",
-                   "Violation type",
-                   "Violation date",
-                   "Place",
-                   "Descriptions" ]
+        self.fields = [ "Ticket no.",
+                        "Violator no.",
+                        "Vehicle id",
+                        "Office no.",
+                        "Violation type",
+                        "Violation date",
+                        "Place",
+                        "Descriptions",
+                        "Exit: Cancel entering violation" ]
         
-        list_of_inputs = [ None for i in range(len(fields)) ]
+        self.list_of_inputs = [ None for i in range(len(self.fields)) ]
 
-        print_field_options( fields, list_of_inputs )
-        choice = get_input( len(fields) )
-        
-        if ( not choice ):
-            return
+        metadata = True
 
-        if ( choice == 1 ):
-            pass 
-        elif ( choice == 2 ):
-            pass
-        elif ( choice == 3 ):
-            pass
-        elif ( choice == 4 ):
-            pass
-        elif ( choice == 5 ):
-            pass
-        elif ( choice == 6 ):
-            pass
-        elif ( choice == 7 ):
-            pass
-        elif ( choice == 8 ):
-            pass
+        while ( True ):
+            self.print_field_options( )
+            choice = self.get_input( len(self.fields) )
+  
+            if ( choice == 1 ):
+                pass 
+            elif ( choice == 2 ):
+                pass
+            elif ( choice == 3 ):
+                pass
+            elif ( choice == 4 ):
+                pass
+            elif ( choice == 5 ):
+                pass
+            elif ( choice == 6 ):
+                pass
+            elif ( choice == 7 ):
+                pass
+            elif ( choice == 8 ):
+                pass
+            # Exit option
+            elif ( choice == 9 ):
+                return
 
-def print_field_options( fields, list_of_inputs ):
-    print( "Enter a field option to edit: " )
-    for i in range( len( fields ) ):
-        print ( "[{:}] ".format( i+1 ) + 
-                fields[i] + 
-                ("" if list_of_inputs[i] else " EMPTY" ) )
+    def print_field_options( self ):
+        print( "Enter a field option to edit: " )
+        for i in range( len( self.fields ) ):
+            print ( "[{:}] ".format( i+1 ) + 
+                    self.fields[i] + 
+                    ("" if self.list_of_inputs[i] else " EMPTY" ) )
 
-# returns False if nothing is entered (user cancels)
-# else returns the integer input choice
-def get_input( num_choices ):
-    print( "Enter a number (Enter nothing to cancel): " )
+    # returns False if nothing is entered (user cancels)
+    # else returns the integer input choice
+    def get_input( self, num_choices ):
+        print( "Enter a number (Enter nothing to cancel): " )
 
-    try:
-        string_input = input()
-        if ( len( string_input ) == 0 ):
-            return False
-        choice = int(string_input)
-    except:
-        choice = "Invalid"
-        
-    while ( type( choice ) is not int 
-            or choice >= num_choices + 1 
-            or choice <= 0 ):
-        print_options()
-        print( "Enter a valid integer choice: " )
         try:
             string_input = input()
-            if ( string_input == 0 ):
+            if ( len( string_input ) == 0 ):
                 return False
             choice = int(string_input)
         except:
             choice = "Invalid"
+        
+        while ( type( choice ) is not int 
+                or choice >= num_choices + 1 
+                or choice <= 0 ):
+            self.print_field_options()
+            print( "Enter a valid integer choice: " )
+            try:
+                string_input = input()
+                if ( string_input == 0 ):
+                    return False
+                choice = int(string_input)
+            except:
+                choice = "Invalid"
 
-    return choice
+        return choice
