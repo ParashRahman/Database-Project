f2fc75d97b09f531fc263968519abad725799f85
ParashRahman <parash@ualberta.ca>
2015-03-21 15:26:21 -0600

    Tested error checker. Should be working for NUMBER (int or float), DATE, and STRING

--- Applications/error_checker.py
+++ Applications/error_checker.py
@@ -25,40 +25,42 @@ class ErrorChecker:
         data_type = meta_data_fragment[1]
 
         # Check the integer type
-        if type(data_type) == cx_Oracle.NUMBER:
+        if data_type == cx_Oracle.NUMBER:
             try:
-                int( user_input )
+                float( user_input )
             except (ValueError): 
                 return False
             
-            precision = meta_data_fragment[4]
+            precision = meta_data_fragment[5]
             
             # integer types should not have any precision
             if ( precision == 0 ):
                 float_user_input = float( user_input )
-                int_user_input = int( user_input )
+                int_user_input = int( float(user_input) )
                 if ( float_user_input > int_user_input ):
                     return False
             # check if float type has proper precision
             else:
                 decimal_user_input = decimal.Decimal( user_input )
                 uinput_precision = decimal_user_input.as_tuple().exponent
-                if ( abs ( uinput_precision ) >= precision ):
+                if ( abs ( uinput_precision ) > precision ):
                     return False
 
+            print ( user_input + " " + str(1) )
+
             # integer types should correspond to the internal size
-            internal_size = meta_data_fragment[3]
-            if ( 10**internal_size < user_input ):
+            internal_size = meta_data_fragment[4]
+            if ( 10**internal_size < int( float(user_input) ) ):
                 return False
 
         # Check string objects
-        elif type(data_type) == cx_Oracle.STRING:
-            desired_length = metad_data_fragment[3]
+        elif data_type == cx_Oracle.STRING:
+            desired_length = meta_data_fragment[3]
             if ( len( user_input ) > desired_length ):
                 return False
             
         # Check datetime objects
-        elif type(data_type) == cx_Oracle.DATETIME:
+        elif data_type == cx_Oracle.DATETIME:
             try:
                 dd = user_input[0]
                 mm = user_input[1]
