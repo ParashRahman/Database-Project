54bd5cbb3016144dad5c7ccdc3ddecfb0e10d84b
ParashRahman <parash@ualberta.ca>
2015-03-22 18:44:27 -0600

    Metadata changes

--- Applications/metadata.py
+++ Applications/metadata.py
@@ -5,52 +5,52 @@ class Metadata:
 
 	def meta_owner(self):
 		self.cursor.execute("select * from owner")
-		self.owner_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_autosale(self):
 		self.cursor.execute("select * from auto_sale")
-		self.auto_sale_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_restriction(self):
 		self.cursor.execute("select * from restriction")
-		self.restriction_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_driving_condition(self):
 		self.cursor.execute("select * from driving_condition")
-		self.driving_condition_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_ticket(self):
 		self.cursor.execute("select * from ticket")
-		self.ticket_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_ticket_type(self):
 		self.cursor.execute("select * from ticket_type")
-		self.ticket_type_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_vehicle(self):
 		self.cursor.execute("select * from vehicle")
-		self.vehicle_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_vehicle_type(self):
 		self.cursor.execute("select * from vehicle_type")
-		self.vehicle_type_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_drive_licence(self):
 		self.cursor.execute("select * from drive_licence")
-		self.drive_licence_metadata=self.cursor.description
+		return self.cursor.description
 
 
 	def meta_people(self):
 		self.cursor.execute("select * from people")
-		self.people_metadata=self.cursor.description
+		return self.cursor.description
 
 
 
