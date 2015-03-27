979f042d0f5a1338219fc0c575be7b8a260ba837
wtianzhi <wtianzhi@gmail.com>
2015-03-27 12:33:00 -0600

    Added Try Excepts to Search

--- Applications/search.py
+++ Applications/search.py
@@ -72,39 +72,42 @@ class Search(Application):
         if( len( user_input ) == 0 ):
             return
 
-        self.cursor.execute("""
-                            SELECT p.name, l.licence_no, p.addr, p.birthday, l.class, r.r_id, l.expiring_date
-                            FROM people p, drive_licence l
-                            LEFT JOIN restriction r
-                            ON l.licence_no = r.licence_no
-                            WHERE p.sin = l.sin
-                            AND (l.licence_no = '{}' OR p.name = '{}')""".format(user_input, user_input))
+        try:
+            self.cursor.execute("""
+                                SELECT p.name, l.licence_no, p.addr, p.birthday, l.class, r.r_id, l.expiring_date
+                                FROM people p, drive_licence l
+                                LEFT JOIN restriction r
+                                ON l.licence_no = r.licence_no
+                                WHERE p.sin = l.sin
+                                AND (l.licence_no = '{}' OR p.name = '{}')""".format(user_input, user_input))
 
-        rows = self.cursor.fetchall()
+            rows = self.cursor.fetchall()
 
-        licence_dict = {}
-        for row in rows:
-            licence_dict[row[1]] = []
-        for row in rows:
-            licence_dict[row[1]].append(row[5])
+            licence_dict = {}
+            for row in rows:
+                licence_dict[row[1]] = []
+            for row in rows:
+                licence_dict[row[1]].append(row[5])
 
-        print("")
-        if len(rows) == 0:
-            print("Driver Not Found")
-        printed = []
-        for row in rows:
-            if row[1] not in printed:
-                print("-------------------------------------------")
-                print("Driver               : " + str(row[0]))
-                print("Licence Number       : " + str(row[1]))
-                print("Driver Address       : " + str(row[2]))
-                print("Driver Birthday      : " + str(row[3]))
-                print("Class                : " + str(row[4]))
-                print("Driving Condition    : " + str(licence_dict[row[1]]))
-                print("Expiration Date      : " + str(row[6]))
-                printed.append(row[1])
-        print("-------------------------------------------")
-        print("")
+            print("")
+            if len(rows) == 0:
+                print("Driver Not Found")
+            printed = []
+            for row in rows:
+                if row[1] not in printed:
+                    print("-------------------------------------------")
+                    print("Driver               : " + str(row[0]))
+                    print("Licence Number       : " + str(row[1]))
+                    print("Driver Address       : " + str(row[2]))
+                    print("Driver Birthday      : " + str(row[3]))
+                    print("Class                : " + str(row[4]))
+                    print("Driving Condition    : " + str(licence_dict[row[1]]))
+                    print("Expiration Date      : " + str(row[6]))
+                    printed.append(row[1])
+            print("-------------------------------------------")
+            print("")
+        except:
+            print("DATABASE ERROR")
 
     def search_violation_history(self):
         """ SEARCH VIOLATION HISTORY DRIVER BY NAME OR ID"""
@@ -113,35 +116,38 @@ class Search(Application):
     
         if( len( user_input ) == 0 ):
             return
-    
-        self.cursor.execute("""
-                            SELECT t.ticket_no, p.name, t.violator_no, t.vehicle_id, p2.name, t.office_no, t.vtype, y.fine, t.vdate, t.place, t.descriptions
-                            FROM ticket t, people p, ticket_type y, people p2, drive_licence l
-                            WHERE p.sin = t.violator_no
-                            AND p2.sin = t.office_no
-                            AND t.vtype = y.vtype
-                            AND l.sin = p.sin
-                            AND (p.sin ='{}' OR l.licence_no ='{}')""".format(user_input,user_input))
+        
+        try:
+            self.cursor.execute("""
+                                SELECT t.ticket_no, p.name, t.violator_no, t.vehicle_id, p2.name, t.office_no, t.vtype, y.fine, t.vdate, t.place, t.descriptions
+                                FROM ticket t, people p, ticket_type y, people p2, drive_licence l
+                                WHERE p.sin = t.violator_no
+                                AND p2.sin = t.office_no
+                                AND t.vtype = y.vtype
+                                AND l.sin = p.sin
+                                AND (p.sin ='{}' OR l.licence_no ='{}')""".format(user_input,user_input))
 
-        rows = self.cursor.fetchall()
-        print("")
-        print("{} Results Found".format(len(rows)))
-        for row in rows:
+            rows = self.cursor.fetchall()
+            print("")
+            print("{} Results Found".format(len(rows)))
+            for row in rows:
+                print("-------------------------------------------")
+                print("Ticket ID            : " + str(row[0]))
+                print("Violator             : " + str(row[1]))
+                print("Violator SIN         : " + str(row[2]))
+                print("Vehicle Serial No    : " + str(row[3]))
+                print("Officer              : " + str(row[4]))
+                print("Officer Sin          : " + str(row[5]))
+                print("Ticket Type          : " + str(row[6]))
+                print("Ticket Fine          : $" + str(row[7]))
+                print("Ticket Date          : " + str(row[8]))
+                print("Ticket Location      : " + str(row[9]))
+                print("Ticket Descriptons   : " + str(row[10]))
+
             print("-------------------------------------------")
-            print("Ticket ID            : " + str(row[0]))
-            print("Violator             : " + str(row[1]))
-            print("Violator SIN         : " + str(row[2]))
-            print("Vehicle Serial No    : " + str(row[3]))
-            print("Officer              : " + str(row[4]))
-            print("Officer Sin          : " + str(row[5]))
-            print("Ticket Type          : " + str(row[6]))
-            print("Ticket Fine          : $" + str(row[7]))
-            print("Ticket Date          : " + str(row[8]))
-            print("Ticket Location      : " + str(row[9]))
-            print("Ticket Descriptons   : " + str(row[10]))
-
-        print("-------------------------------------------")
-        print("")
+            print("")
+        except:
+            print("DATABASE ERROR")
     
     def search_vehicle(self):
         """ SEARCH VEHICLE BY SERIAL NO"""
@@ -150,25 +156,28 @@ class Search(Application):
     
         if( len( user_input ) == 0 ):
             return
-    
-        self.cursor.execute("""
-                            SELECT v.serial_no, COUNT(s.transaction_id), AVG(s.price), COUNT(t.ticket_no)
-                            FROM vehicle v, auto_sale s, ticket t
-                            WHERE v.serial_no = s.vehicle_id 
-                            AND v.serial_no = t.vehicle_id 
-                            AND v.serial_no = '{}'
-                            GROUP BY v.serial_no
-                            """.format(user_input))
+        
+        try:
+            self.cursor.execute("""
+                                SELECT v.serial_no, COUNT(s.transaction_id), AVG(s.price), COUNT(t.ticket_no)
+                                FROM vehicle v, auto_sale s, ticket t
+                                WHERE v.serial_no = s.vehicle_id 
+                                AND v.serial_no = t.vehicle_id 
+                                AND v.serial_no = '{}'
+                                GROUP BY v.serial_no
+                                """.format(user_input))
 
-        rows = self.cursor.fetchall()
+            rows = self.cursor.fetchall()
 
-        print("")
-        print("{} Results Found".format(len(rows)))
-        for row in rows:
+            print("")
+            print("{} Results Found".format(len(rows)))
+            for row in rows:
+                print("-------------------------------------------")
+                print("Serial Number                    : " + row[0])
+                print("Times that vehicle has been sold : " + str(row[1]))
+                print("Average Price                    : $" + str(row[2]))
+                print("Number of violations             : " + str(row[3]))
             print("-------------------------------------------")
-            print("Serial Number                    : " + row[0])
-            print("Times that vehicle has been sold : " + str(row[1]))
-            print("Average Price                    : $" + str(row[2]))
-            print("Number of violations             : " + str(row[3]))
-        print("-------------------------------------------")
-        print("")
+            print("")
+        except:
+            print("DATABASE ERROR")
