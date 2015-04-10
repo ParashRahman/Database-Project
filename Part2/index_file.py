8ecfffc94cd5b3c14b7dfdda43b7126d35708bd0
ParashRahman <parash@ualberta.ca>
2015-04-09 19:09:55 -0600

    push for touqir

---
+++ Part2/index_file.py
@@ -0,0 +1,27 @@
+import bsddb
+
+class IndexFile(DB):
+
+    def __init__(self, db_address):
+        self.inverse_index = {}
+        self.db =  bsddb.btopen(db_address, 'c')
+
+    # OVERRIDED
+    def insert(self, records):
+        for key, value in records:
+            if self.b_tree.db_has_key(key) == False:
+                self.b_tree[key] = value
+                if ( inverse_index.get(value) == None ):
+                    inverse.index[value] = [key]
+                else:
+                    inverse.index[value].append(key)
+
+    #  OVERRIDED
+    def retrieve_using_data_values( self, data_values ):
+        size_db = len( self.db )
+        records = []
+
+        for data_value in data_values:
+            for  count in xrange( size_db ):
+                if count == 0:
+                    current_record = 
